596942b68859522f3a80cbcd8198ef2e3859662b
Praveen-murali <praveenmuraleedhar143@gmail.com>
2021-06-20 15:50:56 +0530

    q4

--- query4.sql
+++ query4.sql
@@ -1,4 +1,4 @@
 select s.name 
-from subjects s , books_subject bs , books b
+from subjects s , books_subjects bs , books b
 where (bs.book = b.id and bs.subject = s.id) and
 b.title = 'Atomic Habits';
