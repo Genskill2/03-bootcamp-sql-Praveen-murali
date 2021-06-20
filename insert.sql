0c7bfd6423f4871112122a5c92f1a94adc1cc685
Praveen-murali <praveenmuraleedhar143@gmail.com>
2021-06-20 14:10:07 +0530

    insrting values

---
+++ insert.sql
@@ -0,0 +1,36 @@
+--inserting values into publisher table
+
+insert into publisher (name , country) values ('PHI' , 'India');
+insert into publisher (name , country) values ('Harper' , 'USA');
+insert into publisher (name , country) values ('GCP' , 'USA');
+insert into publisher (name , country) values ('Avery' , 'USA');
+insert into publisher (name , country) values ('Del Rey' , 'UK');
+insert into publisher (name , country) values ('Vintage' , 'UK');
+
+--inserting values into books table
+
+
+insert into books (title , publisher) values ('The C Programming Language' , 'PHI');
+insert into books (title , publisher) values ('The Go Programming Language' , 'PHI');
+insert into books (title , publisher) values ('The UNIX Programming Environment' , 'PHI');
+insert into books (title , publisher) values ('Cryptonomicon' , 'Harper');
+insert into books (title , publisher) values ('Deep Work' , 'GCP');
+insert into books (title , publisher) values ('Atomic Habit' , 'Avery');
+insert into books (title , publisher) values ('The City And The City' , 'Del Rey');
+insert into books (title , publisher) values ('The Great War for Civilisation' , 'vintage');
+
+
+--insert values into subjects table
+
+insert into subjects (name) values ('C');
+insert into subjects (name) values ('UNIX');
+insert into subjects (name) values ('Technology');
+insert into subjects (name) values ('Go');
+insert into subjects (name) values ('Psychology');
+insert into subjects (name) values ('Productivity');
+insert into subjects (name) values ('Science Fiction');
+insert into subjects (name) values ('Politics');
+insert into subjects (name) values ('History');
+
+
+--insert values into
